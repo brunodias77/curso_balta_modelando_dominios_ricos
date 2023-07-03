321f35ae7972c60b157de88f6c4e58c2d19963f6
brunodias77 <bruno.fera09@gmail.com>
2023-07-03 08:51:10 -0300

    feat: init test

--- PaymentContext.Tests/ValueObjects/DocumentTests.cs
+++ PaymentContext.Tests/ValueObjects/DocumentTests.cs
@@ -11,35 +11,35 @@ namespace PaymentContext.Tests
         // Red, Green, Refactor
 
         [TestMethod]
-        public void ShouldReturnErrorWhenCNPJIsInvalid()
+        public void deve_retornar_erro_quando_o_CNPJ_for_invalido()
         {
             var doc = new Document("123", EDocumentType.CNPJ);
             Assert.IsTrue(doc.Invalid);
         }
 
-        //[TestMethod]
-        //public void ShouldReturnSuccessWhenCNPJIsValid()
-        //{
-        //    var doc = new Document("34110468000150", EDocumentType.CNPJ);
-        //    Assert.IsTrue(doc.Valid);
-        //}
+        [TestMethod]
+        public void deve_retornar_sucesso_quando_o_CNPJ_for_valido()
+        {
+            var doc = new Document("34110468000150", EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Valid);
+        }
 
-        //[TestMethod]
-        //public void ShouldReturnErrorWhenCPFIsInvalid()
-        //{
-        //    var doc = new Document("123", EDocumentType.CPF);
-        //    Assert.IsTrue(doc.Invalid);
-        //}
+        [TestMethod]
+        public void deve_retornar_erro_quando_o_CPF_for_invalido()
+        {
+            var doc = new Document("123", EDocumentType.CPF);
+            Assert.IsTrue(doc.Invalid);
+        }
 
-        //[TestMethod]
-        //[DataTestMethod]
-        //[DataRow("34225545806")]
-        //[DataRow("54139739347")]
-        //[DataRow("01077284608")]
-        //public void ShouldReturnSuccessWhenCPFIsValid(string cpf)
-        //{
-        //    var doc = new Document(cpf, EDocumentType.CPF);
-        //    Assert.IsTrue(doc.Valid);
-        //}
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("34225545806")]
+        [DataRow("54139739347")]
+        [DataRow("01077284608")]
+        public void deve_retornar_sucesso_quando_o_CPF_for_valido(string cpf)
+        {
+            var doc = new Document(cpf, EDocumentType.CPF);
+            Assert.IsTrue(doc.Valid);
+        }
     }
 }
