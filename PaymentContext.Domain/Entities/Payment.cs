7cda26f583d59e9903b9aabc28bfe103213e4279
brunodias77 <brunohdias95@gmail.com>
2023-07-02 23:22:05 -0300

    blabla

--- PaymentContext.Domain/Entities/Payment.cs
+++ PaymentContext.Domain/Entities/Payment.cs
@@ -1,30 +1,37 @@
+using Flunt.Validations;
 using PaymentContext.Domain.ValueObjects;
+using PaymentContext.Shared.Entities;
 
 namespace PaymentContext.Domain.Entities
 {
-  public abstract class Payment
-  {
-    protected Payment(DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, string payer, Document document, Address address, Email email)
+    public abstract class Payment : Entity
     {
-      Number = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 10).ToUpper();
-      PaidDate = paidDate;
-      ExpireDate = expireDate;
-      Total = total;
-      TotalPaid = totalPaid;
-      Payer = payer;
-      Document = document;
-      Address = address;
-      Email = email;
-    }
+        protected Payment(DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, string payer, Document document, Address address, Email email)
+        {
+            Number = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 10).ToUpper();
+            PaidDate = paidDate;
+            ExpireDate = expireDate;
+            Total = total;
+            TotalPaid = totalPaid;
+            Payer = payer;
+            Document = document;
+            Address = address;
+            Email = email;
+            AddNotifications(new Contract<Payment>()
+                .Requires()
+                .IsGreaterThan(0, total, "Payment.Total", "O total nao pode ser 0")
+                .IsGreaterOrEqualsThan(Total, TotalPaid, "Payment.TotalPaid", "O valor pago é menor que o valor do pagamento")
+            );
+        }
 
-    public string Number { get; set; }
-    public DateTime PaidDate { get; set; }
-    public DateTime ExpireDate { get; set; }
-    public decimal Total { get; set; }
-    public decimal TotalPaid { get; set; }
-    public string Payer { get; set; }
-    public Document Document { get; set; }
-    public Address Address { get; set; }
-    public Email Email { get; set; }
-  }
+        public string Number { get; set; }
+        public DateTime PaidDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public decimal Total { get; set; }
+        public decimal TotalPaid { get; set; }
+        public string Payer { get; set; }
+        public Document Document { get; set; }
+        public Address Address { get; set; }
+        public Email Email { get; set; }
+    }
 }
